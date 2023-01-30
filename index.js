dd97211970f3ad9bab539b1a827714843be1b206
codetech18 <akoredealao18@gmail.com>
2023-01-30 13:43:43 -0800

    still in progress

---
+++ index.js
@@ -0,0 +1,87 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const WinConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+let options = ["", "", "", "", "", "", "", ""];
+let currentPlayer = "x";
+let running = false;
+
+
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked()));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`
+    running = true;
+}
+
+function cellClicked(){
+    console.log("ITS WORKING") 
+    const cellIndex = this.document.querySelectorAll('#index');
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner(); 
+}
+
+function updateCell(cell, index){
+    console.log("ITS WORKING") 
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+
+function chanePlayer(){
+    currentPlayer = (currentPlayer=="x") ? "o" : "x";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+
+function checkWinner(){
+    let roundWon = false;
+
+    for(let i = 0; i < WinConditions.length; i++){
+        const condition = WinConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon=true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    } else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false 
+    } else {
+        chanePlayer();
+    }
+}
+
+function restartGame(){
+    currentPlayer = "x";
+    options = ["", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
+}
